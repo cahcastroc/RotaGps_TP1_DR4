a86b07efa163743ecc80f492b39c148b6242c153
Cahcastroc <camila.castronunes@gmail.com>
2022-08-07 20:24:25 -0300

    init

---
+++ app/src/main/java/br/edu/infnet/rotagps_tp1_dr4/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.edu.infnet.rotagps_tp1_dr4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
